990b2632dc70949217c09d793249ea225e71dff4
souheibmadi <sm@aimngt.com>
2023-11-17 17:32:42 +0100

    first commiteEZZ ERT

--- themes/Mytravel/Tour/Views/frontend/layouts/search/form-search.blade.php
+++ themes/Mytravel/Tour/Views/frontend/layouts/search/form-search.blade.php
@@ -1,28 +1,33 @@
-<form action="{{ route("tour.search") }}" class="form bravo_form d-flex mb-1 py-2" method="get">
+<form action="{{ route('tour.search') }}" class="form bravo_form d-flex mb-1 py-2" method="get">
     <div class="g-field-search">
         <div class="row d-block nav-select d-flex align-items-end">
-            @php $tour_search_fields = setting_item_array('tour_search_fields');
-            $tour_search_fields = array_values(\Illuminate\Support\Arr::sort($tour_search_fields, function ($value) {
-                return $value['position'] ?? 0;
-            }));
+            @php$tour_search_fields = setting_item_array('tour_search_fields');
+                $tour_search_fields = array_values(
+                    \Illuminate\Support\Arr::sort($tour_search_fields, function ($value) {
+                        return $value['position'] ?? 0;
+                    }),
+                );
             @endphp
-            @if(!empty($tour_search_fields))
-                @foreach($tour_search_fields as $field)
+            @if (!empty($tour_search_fields))
+                @foreach ($tour_search_fields as $field)
                     @php $field['title'] = $field['title_'.app()->getLocale()] ?? $field['title'] ?? "" @endphp
-                    <div class="col-md-{{ $field['size'] ?? "6" }} mb-4 mb-lg-0 text-left">
+                    <div class="col-md-{{ $field['size'] ?? '6' }} mb-4 mb-lg-0 text-left">
                         @switch($field['field'])
                             @case ('service_name')
                                 @include('Tour::frontend.layouts.search.fields.service_name')
                             @break
+
                             @case ('location')
                                 @include('Tour::frontend.layouts.search.fields.location')
                             @break
+
                             @case ('date')
                                 @include('Tour::frontend.layouts.search.fields.date')
                             @break
-                            {{--@case ('category')
+
+                            {{-- @case ('category')
                                 @include('Tour::frontend.layouts.search.fields.category')
-                            @break--}}
+                            @break --}}
                             @case ('attr')
                                 @include('Tour::frontend.layouts.search.fields.attr')
                             @break
@@ -33,8 +38,9 @@
         </div>
     </div>
     <div class="g-button-submit align-self-lg-end">
-        <button type="submit" class="btn btn-primary btn-md border-radius-3 mb-xl-0 mb-lg-1 transition-3d-hover">
-            <i class="flaticon-magnifying-glass font-size-20 mr-2"></i>{{ __("تابع") }}
-        </button>
+        <a href="{{ url('/registerwizard') }}"
+            class="btn btn-primary btn-md border-radius-3 mb-xl-0 mb-lg-1 transition-3d-hover">
+            <i class="flaticon-magnifying-glass font-size-20 mr-2"></i>{{ __('تابع') }}
+        </a>
     </div>
 </form>
