40fd485664a78fc36d80567dc311d2bd08eaa244
souheibmadi <sm@aimngt.com>
2023-12-10 15:35:45 +0100

    Fix  DeFRTEZZ

--- themes/Mytravel/Template/Views/frontend/blocks/form-search-all-service/style_1.blade.php
+++ themes/Mytravel/Template/Views/frontend/blocks/form-search-all-service/style_1.blade.php
@@ -82,37 +82,35 @@
                             </a>
                         </li>
                     @endif
-                    <li class="nav-item" role="bravo_bravo_morchid">
-                        <a class="nav-link font-weight-medium  @if ($number == 6) active @endif pl-md-5 pl-3 "
-                            id="bravo_bravo_morchid-tab" data-toggle="pill" href="#bravo_bravo_morchid" role="tab"
-                            aria-controls="bravo_morchid" aria-selected="true">
-                            <div
-                                class="d-flex flex-column flex-md-row  position-relative text-white align-items-center">
-                                <figure class="ie-height-40 d-md-block mr-md-3">
-                                    <i class="icon icofont-users-alt-1 font-size-3"></i>
-                                </figure>
-                                <span class="tabtext mt-2 mt-md-0 font-weight-semi-bold">
-                                    مرشد
-                                </span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item" role="bravo_bravo_ticket">
-                        <a class="nav-link font-weight-medium  @if ($number == 7) active @endif pl-md-5 pl-3 "
-                            id="bravo_bravo_ticket-tab" data-toggle="pill" href="#bravo_bravo_ticket" role="tab"
-                            aria-controls="bravo_morchid" aria-selected="true">
-                            <div
-                                class="d-flex flex-column flex-md-row  position-relative text-white align-items-center">
-                                <figure class="ie-height-40 d-md-block mr-md-3">
-                                    <i class="icon icofont-air-ticket font-size-3"></i>
-                                </figure>
-                                <span class="tabtext mt-2 mt-md-0 font-weight-semi-bold">
-                                    تأشيرة
-                                </span>
-                            </div>
-                        </a>
-                    </li>
                 @endforeach
+                <li class="nav-item" role="bravo_bravo_morchid">
+                    <a class="nav-link font-weight-medium  @if ($number == 6) active @endif pl-md-5 pl-3 "
+                        id="bravo_bravo_morchid-tab" data-toggle="pill" href="#bravo_bravo_morchid" role="tab"
+                        aria-controls="bravo_morchid" aria-selected="true">
+                        <div class="d-flex flex-column flex-md-row  position-relative text-white align-items-center">
+                            <figure class="ie-height-40 d-md-block mr-md-3">
+                                <i class="icon icofont-users-alt-1 font-size-3"></i>
+                            </figure>
+                            <span class="tabtext mt-2 mt-md-0 font-weight-semi-bold">
+                                مرشد
+                            </span>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item" role="bravo_bravo_ticket">
+                    <a class="nav-link font-weight-medium  @if ($number == 7) active @endif pl-md-5 pl-3 "
+                        id="bravo_bravo_ticket-tab" data-toggle="pill" href="#bravo_bravo_ticket" role="tab"
+                        aria-controls="bravo_morchid" aria-selected="true">
+                        <div class="d-flex flex-column flex-md-row  position-relative text-white align-items-center">
+                            <figure class="ie-height-40 d-md-block mr-md-3">
+                                <i class="icon icofont-air-ticket font-size-3"></i>
+                            </figure>
+                            <span class="tabtext mt-2 mt-md-0 font-weight-semi-bold">
+                                تأشيرة
+                            </span>
+                        </div>
+                    </a>
+                </li>
             @endif
         </ul>
 
