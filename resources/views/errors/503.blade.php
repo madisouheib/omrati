f254e894cb0a75bc7daedbec749586c1ae9dbcff
souheibmadi <sm@aimngt.com>
2023-12-28 06:36:21 +0100

    add GTYZ

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,10 @@
 @extends('errors::minimal')
-
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;500;600&family=Tajawal:wght@300&display=swap"
+    rel="stylesheet">
+<h2
+    style="margin-top:20%;margin:auto;text-align:center;font-family: 'IBM Plex Sans Arabic', sans-serif;
+font-family: 'Tajawal', sans-serif;">
+    الموقع تحت الصيانة </h2>
