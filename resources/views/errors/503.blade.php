3f41ed26ad81fb25a7ac796999e2d1704fc31f91
souheibmadi <sm@aimngt.com>
2023-12-28 06:37:20 +0100

    add GTYZEEE

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
 <link
     href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;500;600&family=Tajawal:wght@300&display=swap"
     rel="stylesheet">
-<h2
+<h1
     style="margin-top:20%;text-align:center;font-family: 'IBM Plex Sans Arabic', sans-serif;
 font-family: 'Tajawal', sans-serif;">
-    الموقع تحت الصيانة </h2>
+    الموقع تحت الصيانة </h1>
