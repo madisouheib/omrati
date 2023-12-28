b8e214045817d24f559db07a544f0643d684d083
souheibmadi <sm@aimngt.com>
2023-12-28 06:36:51 +0100

    add GTYZE

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,6 +5,6 @@
     href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;500;600&family=Tajawal:wght@300&display=swap"
     rel="stylesheet">
 <h2
-    style="margin-top:20%;margin:auto;text-align:center;font-family: 'IBM Plex Sans Arabic', sans-serif;
+    style="margin-top:20%;text-align:center;font-family: 'IBM Plex Sans Arabic', sans-serif;
 font-family: 'Tajawal', sans-serif;">
     الموقع تحت الصيانة </h2>
