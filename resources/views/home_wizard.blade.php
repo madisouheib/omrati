9a0450eaa2daf50b639c5294dde4a69f40463b6a
souheibmadi <sm@aimngt.com>
2023-12-30 15:06:59 +0100

    add GT EEZZEZ

--- resources/views/home_wizard.blade.php
+++ resources/views/home_wizard.blade.php
@@ -116,7 +116,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 
     <div class="container mt-4" id="app">
         <div class="row justify-content-center">
