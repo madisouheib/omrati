9f915fa00221f678c8aa4bc782aa1942a0b093b7
souheibmadi <sm@aimngt.com>
2023-12-30 15:05:16 +0100

    add GT EEZZE

--- resources/views/home_wizard_tickets.blade.php
+++ resources/views/home_wizard_tickets.blade.php
@@ -112,11 +112,11 @@
             background: #68e870;
         }
     </style>
-
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 
 
     <div class="container mt-4" id="app">
